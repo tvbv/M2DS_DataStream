1a441221a0a114eacc33710c4df60081a82cea27
TitouanLMT <titouan.lermite@gmail.com>
2022-12-09 13:28:29 +0100

    feat: removed encryption which was useless

--- handle_tokens.py
+++ handle_tokens.py
@@ -1,28 +1,11 @@
 from os import path
-from cryptography.fernet import Fernet
-from pwinput import pwinput
 
 
-users = ['Luc', 'Thomas', 'Titouan', 'Other']
 tokens_path = 'tokens.txt'
-fernet_key_path = 'fernet_key.txt'
 
 
-def get_user_name():
-    print('To get started, choose a creator (enter the name or the associated number.)')
-    print(f'The first {len(users) - 1} are the developers of this program.')
-    print(f'If you are not one of them, enter {len(users)} or {users[-1]}.')
-    while True:
-        for i, creator in enumerate(users):
-            print(f'{i+1}. {creator}')
-        choice = input('Your choice: ')
-        if choice.isdigit():
-            choice = int(choice)
-            if 1 <= choice <= len(users):
-                return users[choice-1]
-        elif choice in users:
-            return choice
-        print('Invalid choice, try again.')
+def get_username():
+    return input('To get started, enter your username: ')
 
 
 def query_tokens():
@@ -36,8 +19,6 @@ def query_tokens():
 
 
 def query_token(user):
-    if isinstance(user, int):
-        user = users[user]
     tokens = query_tokens()
     if user in tokens:
         return tokens[user]
@@ -45,57 +26,31 @@ def query_token(user):
         return None
 
 
-def decode_token(encoded_token, fernet_key):
-    return fernet_key.decrypt(encoded_token.encode()).decode()
-
-
-def encode_token(token, fernet_key):
-    return fernet_key.encrypt(token.encode('utf-8')).decode('utf-8')
-
-
 def ask_for_token():
-    tries = 3
-    print('New user dectected.')
-    print(f'Enter the token. You have {tries} tries.')
-    print('(Note: this will be encoded with a password afterwards.)')
-    while tries > 0:
-        token = pwinput('Token: ')
-        token_confirm = pwinput('Confirm the token: ')
-        if token == token_confirm:
-            return token
-        tries -= 1
-        print(f'The tokens do not match, you have {tries} tries left.')
+    return input('New user dectected. Enter your bearer token: ')
 
 
-def add_token(user_name, token_encoded):
+def add_token(username, token_encoded):
     with open(tokens_path, 'a') as f:
-        f.write(f'{user_name}:{token_encoded}')
-
-
-def generate_fernet_key():
-    return Fernet.generate_key().decode('utf-8')
-
-
-def get_fernet_key():
-    if not path.exists(fernet_key_path):
-        with open(fernet_key_path, 'w') as f:
-            key = generate_fernet_key()
-            f.write(key)
-    else:
-        with open(fernet_key_path, 'r') as f:
-            key = f.read()
-    return key
+        f.write(f'{username}:{token_encoded}\n')
 
 
 def get_bearer_token():
-    fernet_key = get_fernet_key()
-    fernet_key = Fernet(fernet_key.encode('utf-8'))
-    user_name = get_user_name()
-    token = query_token(user_name)
+    username = get_username()
+    token = query_token(username)
     if token is None:
         token = ask_for_token()
-        token_encoded = encode_token(token, fernet_key)
-        add_token(user_name, token_encoded)
+        add_token(username, token)
+    return username, token
+
+
+def remove_token(username):
+    tokens = query_tokens()
+    if username in tokens:
+        del tokens[username]
+        with open(tokens_path, 'w') as f:
+            for username, token in tokens.items():
+                f.write(f'{username}:{token}\n')
+        return True
     else:
-        token = decode_token(token, fernet_key)
-    return token
+        return False
