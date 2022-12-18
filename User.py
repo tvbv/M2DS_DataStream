05079a83f2b338af06a8d9f04901ae0dde5ef2e5
TitouanLMT <titouan.lermite@gmail.com>
2022-12-18 13:00:22 +0100

    feat: added default keywords files

--- User.py
+++ User.py
@@ -1,14 +1,6 @@
 import tweepy
 
-
-def add_keywords(keywords):
-    while True:
-        keyword = input('Enter a keyword (leave blank to stop): ')
-        if keyword == '':
-            break
-        else:
-            keywords.append(keyword)
-    return keywords
+from utils import get_keywords
 
 
 class User:
@@ -16,16 +8,15 @@ class User:
         self.client = tweepy.Client(bearer_token=bearer_token)
         self.authenticated = False
         try:
-            for _ in self.get_tweets('test'):  # Test the token
+            for _ in self.get_tweets('COVID'):  # Test the token
                 break  # If the token is valid, we should enter this loop
         except tweepy.errors.Unauthorized:
             raise Exception('Invalid bearer token')
         self.authenticated = True
-        self.default_keywords = ['SNCF', 'retard']
 
     def get_tweets(self, query=None, max_tweets=100, tweet_fields=None):
         if query is None:
-            query = self.ask_keywords()
+            query = get_keywords()
         if isinstance(query, list):
             query = ' OR '.join(query)
         if tweet_fields is None:
@@ -33,34 +24,3 @@ class User:
         for tweet in tweepy.Paginator(self.client.search_recent_tweets, query=query, max_results=max_tweets,
                                       tweet_fields=tweet_fields).flatten():
             yield tweet
-
-    def ask_keywords(self):
-        while True:
-            print(f'Default keywords: {self.default_keywords}')
-            print('Select an option:')
-            print('1. Add keywords')
-            print('2. Create your own list of keywords')
-            print('3. Do nothing (use default keywords)')
-            option = input('Option: ')
-            try:
-                option = int(option)
-                if option < 1 or option > 3:
-                    print('Invalid option. Try again.')
-                    continue
-                break
-            except:
-                print('Invalid option. Try again.')
-        if option == 1:
-            keywords = self.default_keywords
-            keywords = add_keywords(keywords)
-        if option == 2:
-            keywords = []
-            while True:
-                keywords = add_keywords(keywords)
-                if len(keywords) > 0:
-                    break
-                else:
-                    print('You must enter at least one keyword.')
-        if option == 3:
-            keywords = self.default_keywords
-        return keywords
--- ingest_tweets.py
+++ ingest_tweets.py
@@ -1,30 +1,20 @@
-from handle_tokens import get_bearer_token, add_token
 from User import User
 import time
 from kafka import KafkaProducer
-from utils import get_kafka_topic
-
+from utils import get_kafka_topic, get_bearer_token
 
 if __name__ == '__main__':
-    username, token, new_user = get_bearer_token()
+    token = get_bearer_token()
     user = User(token)
     if not user.authenticated:
         print('Invalid bearer token')
         exit()
-    if new_user:
-        add_token(username, token)
     kafka_topic = get_kafka_topic('raw_tweets_dspp')
     # dspp: Data Stream Processing Project
     port = '9092'
     producer = KafkaProducer(bootstrap_servers=f'localhost:{port}')
-    print(f'Producing tweets to topic {kafka_topic}...')
-    all_tweet_fields = ['data', 'id', 'text', 'edit_history_tweet_ids', 'attachments', 'author_id',
-                        'context_annotations', 'conversation_id', 'created_at', 'edit_controls', 'entities',
-                        'geo', 'in_reply_to_user_id', 'lang', 'non_public_metrics', 'organic_metrics',
-                        'possibly_sensitive', 'promoted_metrics', 'public_metrics', 'referenced_tweets',
-                        'reply_settings', 'source', 'withheld']
-    tweet_fields = ['id', 'text', 'author_id', 'created_at', 'public_metrics', 'lang', 'non_public_metrics',
-                    'organic_metrics', 'possibly_sensitive', 'promoted_metrics', 'public_metrics']
-    for tweet in user.get_tweets(tweet_fields=tweet_fields):
-        producer.send(kafka_topic, tweet.json().encode('utf-8'))
+    for tweet in user.get_tweets():
+        if tweet.lang == 'en':
+            print(tweet.text)
+            producer.send(kafka_topic, tweet.json().encode('utf-8'))
         time.sleep(1)
